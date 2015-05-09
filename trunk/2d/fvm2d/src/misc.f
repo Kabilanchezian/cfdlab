3a5151ff9c1dbcca13b3c3484e12494e944ff6cf
cpraveen@gmail.com <cpraveen@gmail.com@9aad49a7-c427-0410-9c06-e9617355c078>
2015-05-09 16:37:40 +0000

    Moved to github

--- trunk/2d/fvm2d/src/misc.f
+++
@@ -1,9 +0,0 @@
-      subroutine banner
-
-      print*,'************************************************'
-      print*,'* 2-D Finite Volume Code on Triangular Grids   *'
-      print*,'*             written by Praveen. C            *'
-      print*,'************************************************'
-
-      return
-      end
